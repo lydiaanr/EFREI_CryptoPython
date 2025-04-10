b98eb7a2543c56320ee2e639d07f11d0478094c9
lydiaanr <121069088+lydiaanr@users.noreply.github.com>
2025-04-10 17:49:02 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') # commentaire2é
+    return render_template('hello.html') # commentaire22
 
 key = Fernet.generate_key()
 f = Fernet(key)
